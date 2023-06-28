181e0c799bd4ba27a98722d8966dff785facf770
manas-singh19 <41261719+manas-singh19@users.noreply.github.com>
2023-06-28 18:22:41 +0530

    WAVEFORM SETUP

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './style.css';
 import STT from './stt.js';
 import App from './recording';
+import Waveform from './waveform';
 export default function App() {
   return (
     <div>
-      <STT />
+      <App />
     </div>
   );
 }
