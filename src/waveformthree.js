e947aa929753068c68c5485f5c2185d3b37dd8d7
manas-singh19 <41261719+manas-singh19@users.noreply.github.com>
2023-06-29 18:20:55 +0530

    1 file modified, 1 added, 1 deleted

--- src/waveformthree.js
+++ src/waveformthree.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import Waveform from './component/waveformthreeCOMP.js';
-import pianoClip from './piano.mp3';
+import Song from './song.mp3';
 
 const WaveformThree = () => {
   return (
     <main style={{ padding: '16px' }}>
       <h1>WaveSurfer Demo</h1>
-      <Waveform audio={pianoClip} />
+      <Waveform audio={Song} />
     </main>
   );
 };
