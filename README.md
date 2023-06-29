ee11d891bad342be68e9cc9191a21c6465f070b6
manas-singh19 <41261719+manas-singh19@users.noreply.github.com>
2023-06-29 18:08:43 +0530

    2 files modified, 1 added

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ https://github.com/samhirtarif/react-audio-visualize
 # - waveform
 
 https://github.com/youneszahzouh/waveform-audio-recorder#readme
+
+## play audio and show waveform
+
+https://github.com/andreidobrinski/react-wavesurfer-demo
+
+## --- not try yet
+
+https://wavesurfer-js.org/examples/#record.js
--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import STT from './stt.js';
 import App from './recording';
 import Waveform from './waveform';
 import WaveformTwo from './waveformtwo';
+import WaveformThreeAudio from './waveformthreeWithAudio';
 import ClassHTML from './classHtml';
 export default function App() {
   return (
     <div>
-      <WaveformTwo />
+      <WaveformThreeAudio />
     </div>
   );
 }
